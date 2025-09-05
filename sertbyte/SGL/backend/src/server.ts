be1c2f27a76ac5211b1c535185d5e5ed5ba320cc
Carlos Batista <123827773+carlosbatistta@users.noreply.github.com>
2025-09-04 23:09:23 -0300

    ajustes do express e cors

--- sertbyte/SGL/backend/src/server.ts
+++ sertbyte/SGL/backend/src/server.ts
@@ -1,11 +1,21 @@
 import express from 'express'
-import type { Request, Response } from 'express';
-import { router } from './routes.js';
+import type { Request, Response } from 'express'
+import { router } from './routes.js'
+import 'express-async-errors' 
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
 app.use(router)
+app.use(cors());
+app.use((err: Error, req: Request, res: Response): void => {
+  if (err instanceof Error) {
+    res.status(400).json({ error: err.message });
+  } else {
+    res.status(500).json({ status: 'error', message: 'Internal server error.' });
+  }
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World');
