6b8fc414a65795accc9e2c4c4dca158fda1f7397
Carlos Batista <123827773+carlosbatistta@users.noreply.github.com>
2025-09-07 07:29:43 -0300

    add jwt

--- sertbyte/Sertgl/backend/src/routes.ts
+++ sertbyte/Sertgl/backend/src/routes.ts
@@ -1,5 +1,9 @@
 import { Router, type Request, type Response } from 'express';
 
+import { CreateUserController } from './controller/user/CreateUserController'
+import { AuthUserController } from './controller/user/AuthUserController'
+import { DetailuserController } from './controller/user/DetailUserController'
+
 const router = Router()
 
 router.get('/teste', (req: Request, res: Response) => {
